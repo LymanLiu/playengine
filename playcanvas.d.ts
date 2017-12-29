b7f3fc221ba95f4b09e393130db87cdcfa46c9b5
scarletsky <scarletsky1025@gmail.com>
2017-12-29 17:59:21 +0800

    feat: improve example

---
+++ playcanvas.d.ts
@@ -0,0 +1,15 @@
+declare module playcanvas {
+    export class Application {
+        constructor(canvas : HTMLCanvasElement, options : any);
+        start: () => void;
+        setCanvasFillMode: (mode: string) => void;
+        setCanvasResolution: (mode: string) => void;
+        on: (event: string, callback : () => void) => void;
+    }
+
+    export const FILLMODE_NONE: string;
+    export const FILLMODE_FILL_WINDOW: string;
+    export const FILLMODE_KEEP_ASPECT: string;
+    export const RESOLUTION_AUTO: string;
+    export const RESOLUTION_FIXED: string;
+}
