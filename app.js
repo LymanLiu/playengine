3ec5048bf4cd4df3ee3525deabaaa29e7f7e1171
scarletsky <scarletsky1025@gmail.com>
2017-12-29 18:36:19 +0800

    chore: improve example

--- app.js
+++ app.js
@@ -1,2 +1,19 @@
 var canvas = document.getElementById('application');
-var app = new playengine.Application(canvas, {});
+var app = new pe.Application(canvas, {});
+var camera = new pe.Camera({
+    position: [0, 0, 10]
+});
+var cube = new pe.Model({
+    position: [0, 0, 0],
+    rotation: [45, 45, 45],
+    type: 'box'
+});
+var light = new pe.Light({
+    position: [1, 1, 1],
+    rotation: [45, 45, 45],
+    type: 'point'
+});
+
+app.$.root.addChild(camera.entity);
+app.$.root.addChild(cube.entity);
+app.$.root.addChild(light.entity);
