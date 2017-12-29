d0aa26a152d72e5920ab5feccbd99cb9d89744c6
scarletsky <scarletsky1025@gmail.com>
2017-12-29 18:33:18 +0800

    chore: rename playengine to pe in umd module

--- rollup.config.js
+++ rollup.config.js
@@ -5,7 +5,7 @@ export default {
     input: 'src/index.ts',
     output: [
         { file: 'dist/playengine.js', format: 'cjs' },
-        { file: 'dist/playengine.umd.js', format: 'umd', name: 'playengine' },
+        { file: 'dist/playengine.umd.js', format: 'umd', name: 'pe' },
         { file: 'dist/playengine.es.js', format: 'es' }
     ],
     plugins: [
