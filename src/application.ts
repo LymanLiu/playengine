16e03cc5e2cf72e6c7a2fbd71c841370aa09e990
scarletsky <scarletsky1025@gmail.com>
2017-12-29 18:20:01 +0800

    chore: rename pcApp to $

--- src/application.ts
+++ src/application.ts
@@ -9,16 +9,16 @@ export interface ApplicationOptions {
 }
 
 export class Application {
-    pcApp : any;
+    $ : any;
 
     constructor(canvas : HTMLCanvasElement, options : ApplicationOptions = {}) {
-        this.pcApp = new pc.Application(canvas, options);
-        this.pcApp.graphicsDevice.maxPixelRatio = window.devicePixelRatio;
-        this.pcApp.setCanvasFillMode(pc.FILLMODE_FILL_WINDOW);
-        this.pcApp.setCanvasResolution(pc.RESOLUTION_AUTO);
-        this.pcApp.loader._handlers.texture.crossOrigin = true;
+        this.$ = new pc.Application(canvas, options);
+        this.$.graphicsDevice.maxPixelRatio = window.devicePixelRatio;
+        this.$.setCanvasFillMode(pc.FILLMODE_FILL_WINDOW);
+        this.$.setCanvasResolution(pc.RESOLUTION_AUTO);
+        this.$.loader._handlers.texture.crossOrigin = true;
 
-        this.pcApp.start();
+        this.$.start();
     }
 }
 
