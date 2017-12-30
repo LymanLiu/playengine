a75c2668b8567294bd6fee1f1ee5558c12a98a93
scarletsky <scarletsky1025@gmail.com>
2017-12-30 15:06:47 +0800

    chore: adjust example

--- examples/app.js
+++ examples/app.js
@@ -1,18 +1,26 @@
 var canvas = document.getElementById('application');
 var app = new pe.Application(canvas, {});
+var orbitCamera = new pe.scripts.OrbitCamera(app.$);
+
 var camera = new pe.Camera({
+    name: 'camera',
     position: [0, 0, 10]
 });
 var cube = new pe.Model({
+    name: 'cube',
     position: [0, 0, 0],
     rotation: [45, 45, 45],
     type: 'box'
 });
 var light = new pe.Light({
+    name: 'light',
     position: [1, 1, 1],
     rotation: [45, 45, 45],
     type: 'point'
 });
+
+camera.entity.addComponent('script');
+camera.entity.script.create(orbitCamera.name);
 
 app.$.root.addChild(camera.entity);
 app.$.root.addChild(cube.entity);
