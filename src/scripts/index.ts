e8d9606f1abfd78fe6e7ba94b2f36b0c4d4d813e
scarletsky <scarletsky1025@gmail.com>
2018-01-01 14:11:18 +0800

    feat: add orbitCameraTouchInput

--- src/scripts/index.ts
+++ src/scripts/index.ts
@@ -1,9 +1,11 @@
 import { script } from './script';
 import { OrbitCamera } from './orbitCamera';
 import { OrbitCameraMouseInput } from './orbitCameraMouseInput';
+import { OrbitCameraTouchInput } from './orbitCameraTouchInput';
 
 export const scripts = {
     script,
     OrbitCamera,
-    OrbitCameraMouseInput
+    OrbitCameraMouseInput,
+    OrbitCameraTouchInput
 }
