ef30469bb47b55402dc31e07d016b8357d460eaf
scarletsky <scarletsky1025@gmail.com>
2018-01-09 15:08:27 +0800

    feat: add serve plugin for rollup

--- rollup.config.js
+++ rollup.config.js
@@ -1,13 +1,18 @@
 import typescript from 'rollup-plugin-typescript';
+import serve from 'rollup-plugin-serve';
 import json from 'rollup-plugin-json';
 
 export default {
     input: 'src/index.ts',
     output: [
         { file: 'dist/playengine.js', format: 'cjs' },
-        { file: 'dist/playengine.umd.js', format: 'umd', name: 'pe' },
+        { file: 'examples/dist/playengine.umd.js', format: 'umd', name: 'pe' },
     ],
     plugins: [
+        serve({
+            contentBase: 'examples/',
+            port: 3333
+        }),
         typescript({
             typescript: require('typescript')
         }),
