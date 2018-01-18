ce66d3f76886caa973cba953555185b324c61b30
scarletsky <scarletsky1025@gmail.com>
2018-01-19 00:36:17 +0800

    refactor: improve builds

--- rollup.config.js
+++ rollup.config.js
@@ -1,28 +1,81 @@
 import typescript from 'rollup-plugin-typescript';
+import commonjs from 'rollup-plugin-commonjs';
+import resolve from 'rollup-plugin-node-resolve';
+import inject from 'rollup-plugin-inject';
 import serve from 'rollup-plugin-serve';
 import json from 'rollup-plugin-json';
 
-let extraPlugins = [];
+const builds = {
+    dev: {
+        output: {
+            file: 'examples/dist/playengine.umd.js',
+            format: 'umd',
+            name: 'pe',
+            globals: {
+                '@scarletsky/playcanvas': 'pc'
+            }
+        },
+        external: [
+            '@scarletsky/playcanvas'
+        ],
+        plugins: [
+            inject({
+                pc: '@scarletsky/playcanvas'
+            }),
+            resolve(),
+            commonjs()
+        ]
+    },
+    cjs: {
+        output: {
+            file: 'dist/playengine.js',
+            format: 'cjs'
+        }
+    },
+    umd: {
+        output: {
+            file: 'dist/playengine.umd.js',
+            format: 'umd',
+            name: 'pe',
+            globals: {
+                '@scarletsky/playcanvas': 'pc'
+            }
+        },
+        external: [
+            '@scarletsky/playcanvas'
+        ],
+        plugins: [
+            inject({
+                pc: '@scarletsky/playcanvas'
+            }),
+            resolve(),
+            commonjs()
+        ]
+    }
+};
 
-if (process.env.NODE_ENV === 'development') {
-    extraPlugins.push(
-        serve({
+function genConfig(name) {
+    const opts = builds[name];
+    const config = {
+        input: 'src/index.ts',
+        output: opts.output,
+        external: opts.external,
+        plugins: [
+            typescript({
+                typescript: require('typescript')
+            }),
+            json()
+        ].concat(opts.plugins || [])
+    };
+
+    if (name === 'dev') {
+        config.plugins.push(serve({
             contentBase: 'examples/',
             port: 3333
-        })
-    );
+        }));
+    }
+
+    return config;
 }
 
-export default {
-    input: 'src/index.ts',
-    output: [
-        { file: 'dist/playengine.js', format: 'cjs' },
-        { file: 'examples/dist/playengine.umd.js', format: 'umd', name: 'pe' },
-    ],
-    plugins: [
-        typescript({
-            typescript: require('typescript')
-        }),
-        json()
-    ].concat(extraPlugins)
-}
+export default genConfig(process.env.TARGET);
