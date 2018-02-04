8d26e24045ae0c46a90d7a760f75a4adfb8e6a56
scarletsky <scarletsky1025@gmail.com>
2018-02-04 15:52:47 +0800

    chore: add constants for entity types

--- src/constants.ts
+++ src/constants.ts
@@ -1,8 +1,8 @@
-var MATERIAL_COLOR_FIELDS = [
+let MATERIAL_COLOR_FIELDS = [
     "diffuse", "specular", "ambient", "emissive"
 ];
 
-var MATERIAL_VECTOR_FIELDS = [
+let MATERIAL_VECTOR_FIELDS = [
     // vec2
     "diffuseMapOffset", "diffuseMapTiling",
     "opacityMapOffset", "opacityMapTiling",
@@ -16,36 +16,45 @@ var MATERIAL_VECTOR_FIELDS = [
     "lightMapOffset", "lightMapTiling",
 ];
 
-export var MATERIAL_OBJECT_FIELDS = [
+export let MATERIAL_OBJECT_FIELDS = [
     "cubeMapProjectionBox"
 ];
-export var MATERIAL_OBJECT_FIELDS2 = MATERIAL_OBJECT_FIELDS.reduce(function(prev : any, next : string) {
+export let MATERIAL_OBJECT_FIELDS2 = MATERIAL_OBJECT_FIELDS.reduce((prev: any, next: string) => {
     prev[next] = true;
     return prev;
 }, {});
 
-export var MATERIAL_ARRAY_FIELDS = MATERIAL_COLOR_FIELDS.concat(MATERIAL_VECTOR_FIELDS);
-export var MATERIAL_ARRAY_FIELDS2 = MATERIAL_ARRAY_FIELDS.reduce(function(prev : any, next : string) {
+export let MATERIAL_ARRAY_FIELDS = MATERIAL_COLOR_FIELDS.concat(MATERIAL_VECTOR_FIELDS);
+export let MATERIAL_ARRAY_FIELDS2 = MATERIAL_ARRAY_FIELDS.reduce((prev: any, next: string) => {
     prev[next] = true;
     return prev;
 }, {});
 
-export var MATERIAL_TEXTURE_FIELDS = [
+export let MATERIAL_TEXTURE_FIELDS = [
     "aoMap", "diffuseMap", "glossMap", "metalnessMap",
     "specularMap", "emissiveMap", "opacityMap", "lightMap",
     "normalMap", "heightMap", "sphereMap", "cubeMap"
 ];
 
-export var MATERIAL_TEXTURE_FIELDS2 = MATERIAL_TEXTURE_FIELDS.reduce(function(prev : any, next : string) {
+export let MATERIAL_TEXTURE_FIELDS2 = MATERIAL_TEXTURE_FIELDS.reduce((prev: any, next: string) => {
     prev[next] = true;
     return prev;
 }, {});
 
+export let ENTITY_BASE = "entity";
+export let ENTITY_CAMERA = "camera";
+export let ENTITY_LIGHT = "light";
+export let ENTITY_MODEL = "model";
+
 export default {
+    ENTITY_BASE,
+    ENTITY_CAMERA,
+    ENTITY_LIGHT,
+    ENTITY_MODEL,
     MATERIAL_OBJECT_FIELDS,
     MATERIAL_OBJECT_FIELDS2,
     MATERIAL_ARRAY_FIELDS,
     MATERIAL_ARRAY_FIELDS2,
     MATERIAL_TEXTURE_FIELDS,
     MATERIAL_TEXTURE_FIELDS2
-}
+};
